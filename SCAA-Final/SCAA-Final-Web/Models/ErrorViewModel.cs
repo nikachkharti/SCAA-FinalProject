599d13815a87990127f9eab14c3d8bb2f5d81ff5
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2026-09-12 09:42:46 +0400

    Template project

---
+++ SCAA-Final/SCAA-Final-Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SCAA_Final_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
